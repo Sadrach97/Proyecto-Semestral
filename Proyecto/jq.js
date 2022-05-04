fb25ed04e6bb503c1c8f02c6c93e157c469799fc
Sadrach Cuevas <sa.cuevas@duocuc.cl>
2022-05-03 21:29:23 -0400

    cambios no me acuerdo

--- Proyecto/jq.js
+++ Proyecto/jq.js
@@ -1,42 +1,25 @@
-$(document).ready(function() {
-    $("#basic-form").validate({
-      rules: {
-        nombre : {
-          required: true,
-          minlength: 3
-        }, appate : {
-            required: true,
-            minlength: 3
-          }
-        ,
-        Correo: {
-          required: true,
-          email: true
-        },
-        weight: {
-          required: {
-            depends: function(elem) {
-              return $("#age").val() > 50
-            }
-          },
-          number: true,
-          min: 0
-        }
-      },
-      messages : {
-        nombre: {
-          minlength: "El nombre debe tener al menos 3 caracteres"
-        },
-        appate: {
-            minlength: "El apellido debe tener al menos 3 caracteres"
-          },
-        Correo: {
-          email: "El correo debe ser como el siguiente formato: abc@stylish.com"
-        },
-        weight: {
-          required: "People with age over 50 have to enter their weight",
-          number: "Please enter your weight as a numerical value"
-        }
+const formulario = document.getElementById('form');
+const inputs = document.querySelectorAll('#form control');
+
+
+
+
+
+
+function comprobarClave(){
+  clave1 = document.f1.clave1.value
+  clave2 = document.f1.clave2.value
+
+  if (clave1 !== clave2)
+      {
+        alert("Las claves son distintas...")
+        document.f1.clave1.value = "";
+        document.f1.clave2.value = "";
+        document.f1.clave1.value.focus();
       }
-    });
-  });
+  else
+     pass
+     
+     
+}
+
