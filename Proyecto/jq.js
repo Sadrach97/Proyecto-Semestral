e1a5bfbd87b6d6f95ecf8bafe6c971f8580392f4
Sadrach Cuevas <sa.cuevas@duocuc.cl>
2022-05-04 01:07:58 -0400

    cambios xd

--- Proyecto/jq.js
+++ Proyecto/jq.js
@@ -1,7 +1,8 @@
 const formulario = document.getElementById('form');
 const inputs = document.querySelectorAll('#form control');
 
-
+const formulario_1 = document.getElementById('form1');
+const inputs1 = document.getElementById('#form control1');
 
 
 
@@ -21,5 +22,9 @@ function comprobarClave(){
      pass
      
      
+}
+function rellenarDatos(){
+  nome = document.f2.nombre1.value = document.f1.nombre.value;
+  ape = document.f2.appate1.value = document.f1.appate.value;
 }
 
